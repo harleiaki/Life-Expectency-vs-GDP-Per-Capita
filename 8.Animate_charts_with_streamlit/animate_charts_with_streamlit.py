b8a8cad30fdef98eb878d82b5fec14c5bc958a66
HAKI <96266332+harleiaki@users.noreply.github.com>
2023-08-17 16:29:34 -0300

    Update animate_charts_with_streamlit.py

--- 8.Animate_charts_with_streamlit/animate_charts_with_streamlit.py
+++ 8.Animate_charts_with_streamlit/animate_charts_with_streamlit.py
@@ -20,7 +20,7 @@ df=gapminder[gapminder['year']==years_select]
 # fig = px.scatter(df, x='gdpPercap', y='lifeExp', color='continent',hover_name='continent',log_x=True,size='pop',hover_name='country',size_max=55,range_x=[100,100000],range_y=[25,90])
 fig = px.scatter(gapminder, x='gdpPercap', y='lifeExp', color='continent', size='pop', size_max=40, 
                 hover_name='country', log_x=True, animation_frame='year',
-                 animation_group='country', range_x=[25, 10000], range_y=[25,90])
+                 animation_group='country', range_x=[100, 10000], range_y=[25,90])
 fig.update_layout(width=900)
 st.header("Customized Plot for " + str(years_select) )
 st.write(fig)
@@ -29,7 +29,7 @@ st.write(fig)
 # fig1 = px.scatter(gapminder, x='gdpPercap', y='lifeExp', color='continent',hover_name='continent',log_x=True,size='pop',size_max=55,range_x=[100,100000],range_y=[25,90],animation_frame="year",animation_group="country")
 fig1 = px.scatter(gapminder, x='gdpPercap', y='lifeExp', color='continent', size='pop', size_max=40, 
                 hover_name='country', log_x=True, animation_frame='year',
-                 animation_group='country', range_x=[25, 10000], range_y=[25,90])
+                 animation_group='country', range_x=[100, 10000], range_y=[25,90])
 fig1.update_layout(width=900)
 st.header("Demo of an animated plot")
 st.write(fig1)
