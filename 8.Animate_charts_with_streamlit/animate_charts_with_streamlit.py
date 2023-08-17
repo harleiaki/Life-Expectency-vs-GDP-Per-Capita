692f818726ee2d5db22642af2f7938581bc2915e
HAKI <96266332+harleiaki@users.noreply.github.com>
2023-08-17 16:34:52 -0300

    Update animate_charts_with_streamlit.py

--- 8.Animate_charts_with_streamlit/animate_charts_with_streamlit.py
+++ 8.Animate_charts_with_streamlit/animate_charts_with_streamlit.py
@@ -17,7 +17,7 @@ years_select=st.selectbox('select year for data',years,0)
 
 df=gapminder[gapminder['year']==years_select] 
 # color by continent 
-fig = px.scatter(df, x='gdpPercap', y='lifeExp', color='continent', hover_name='continent',log_x=True,size='pop',hover_name='country',size_max=55,range_x=[100,100000],range_y=[25,90])
+fig = px.scatter(df, x='gdpPercap', y='lifeExp', color='continent',log_x=True,size='pop',hover_name='country',size_max=55,range_x=[100,100000],range_y=[25,90])
 fig.update_layout(width=900)
 st.header("Customized Plot for " + str(years_select) )
 st.write(fig)
